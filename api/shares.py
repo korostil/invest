888ed612b68d4c26511f38ebac40653d07c4e840
korostil <av.korostil@gmail.com>
2021-12-27 07:11:09 +0300

    Use multiple form

--- api/shares.py
+++ api/shares.py
@@ -9,7 +9,7 @@ from services.shares import share
 router = APIRouter()
 
 
-@router.get('/share/', tags=['shares'])
+@router.get('/shares/', tags=['shares'])
 async def get_list_of_shares(
     limit: int = Query(25, gt=0, le=100), offset: int = Query(0, ge=0)
 ) -> APIResponse:
@@ -22,7 +22,7 @@ async def get_list_of_shares(
     )
 
 
-@router.get('/share/{ticker}/', tags=['shares'])
+@router.get('/shares/{ticker}/', tags=['shares'])
 async def get_share(ticker: str = Query(...)) -> APIResponse:
     share_document = await share.get(ticker)
     if not share_document:
@@ -30,7 +30,7 @@ async def get_share(ticker: str = Query(...)) -> APIResponse:
     return APIResponse(ShareResponse(**share_document.dump()))
 
 
-@router.post('/share/', tags=['shares'])
+@router.post('/shares/', tags=['shares'])
 async def create_share(share_data: ShareCreate) -> APIResponse:
     with funcy.reraise(ValidationError, UnprocessableEntity):
         share_document = await share.create(document=share_data)
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -10,10 +10,10 @@ from tests.helpers import url_path_for
 @pytest.mark.parametrize(
     'name,query_params,path_params,expected',
     [
-        ('get_list_of_shares', {}, {}, '/api/share/'),
-        ('get_list_of_shares', {'limit': 10}, {}, '/api/share/?limit=10'),
-        ('get_share', {}, {'ticker': 'TSLA'}, '/api/share/TSLA/'),
-        ('get_share', {'limit': 10}, {'ticker': 'TSLA'}, '/api/share/TSLA/?limit=10'),
+        ('get_list_of_shares', {}, {}, '/api/shares/'),
+        ('get_list_of_shares', {'limit': 10}, {}, '/api/shares/?limit=10'),
+        ('get_share', {}, {'ticker': 'TSLA'}, '/api/shares/TSLA/'),
+        ('get_share', {'limit': 10}, {'ticker': 'TSLA'}, '/api/shares/TSLA/?limit=10'),
     ],
 )
 def test_url_path_for(name, query_params, path_params, expected):
